a38d931554ac3275bceaf3b8caa04e9338bb059c
zanuccod <54598430+zanuccod@users.noreply.github.com>
2020-01-08 21:53:23 +0100

    Add IdentityServer4 test project

---
+++ WebApi/Services/Identity/IdentityServer.API.Test/Controllers/AccountControllerTest.cs
@@ -0,0 +1,102 @@
+﻿using System.Threading.Tasks;
+using IdentityServer.API.Controllers;
+using IdentityServer.API.Domains;
+using IdentityServer.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using NUnit.Framework;
+
+namespace IdentityServer.API.Test.Controllers
+{
+    [TestFixture]
+    public class AccountControllerTest
+    {
+        private readonly Mock<IUserDataModel> mockUserDataModel;
+        private readonly AccountController controller;
+
+        public AccountControllerTest()
+        {
+            mockUserDataModel = new Mock<IUserDataModel>();
+            var userDataModel = mockUserDataModel.Object;
+
+            controller = new AccountController(userDataModel, new NullLogger<AccountController>());
+        }
+
+        [Test]
+        public async Task RegisterUser_NullUser_ShouldReturnBadRequest()
+        {
+            // Act
+            var result = await controller.RegisterUser(null);
+
+            // Assert
+            Assert.AreEqual(typeof(BadRequestObjectResult), result.GetType());
+        }
+
+        [Test]
+        public async Task RegisterUser_AlreadyExistUser_ShouldReturnConflict()
+        {
+            // Arrange
+            // moq data model to return an already existing user
+            mockUserDataModel.Setup(x => x.GetUserAsync(It.IsAny<string>())).Returns(Task.FromResult(new User()));
+
+            // Act
+            var result = await controller.RegisterUser(new User());
+
+            // Assert
+            Assert.AreEqual(typeof(ConflictObjectResult), result.GetType());
+        }
+
+        [Test]
+        public async Task RegisterUser_NewUser_ShouldReturnOk()
+        {
+            // Arrange
+            // moq data model to return an already existing user
+            mockUserDataModel.Setup(x => x.GetUserAsync(It.IsAny<string>())).Returns(Task.FromResult<User>(null));
+
+            // Act
+            var result = await controller.RegisterUser(new User());
+
+            // Assert
+            Assert.AreEqual(typeof(OkObjectResult), result.GetType());
+        }
+
+        [Test]
+        public async Task DeleteUser_NullUser_ShouldReturnBadRequest()
+        {
+            // Act
+            var result = await controller.DeleteUser(null);
+
+            // Assert
+            Assert.AreEqual(typeof(BadRequestObjectResult), result.GetType());
+        }
+
+        [Test]
+        public async Task DeleteUser_UserNotExist_ShouldReturnNotFound()
+        {
+            // Arrange
+            // moq data model to return an already existing user
+            mockUserDataModel.Setup(x => x.GetUserAsync(It.IsAny<string>())).Returns(Task.FromResult<User>(null));
+
+            // Act
+            var result = await controller.DeleteUser("demo");
+
+            // Assert
+            Assert.AreEqual(typeof(NotFoundObjectResult), result.GetType());
+        }
+
+        [Test]
+        public async Task RegisterUser_UserExsits_ShouldReturnOk()
+        {
+            // Arrange
+            // moq data model to return an already existing user
+            mockUserDataModel.Setup(x => x.GetUserAsync(It.IsAny<string>())).Returns(Task.FromResult(new User()));
+
+            // Act
+            var result = await controller.DeleteUser("demo");
+
+            // Assert
+            Assert.AreEqual(typeof(OkObjectResult), result.GetType());
+        }
+    }
+}
---
+++ WebApi/Services/Identity/IdentityServer.API.Test/Models/EFUserDataModelTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using IdentityServer.API.Models;
+using IdentityServer.API.Domains;
+
+namespace IdentityServer.API.Test.Models
+{
+    [TestFixture]
+    public class EFUserDataModelTest
+    {
+        private EFUserDataModel dataModel;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            var options = new DbContextOptionsBuilder()
+                            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                            .Options;
+            dataModel = new EFUserDataModel(options);
+        }
+
+        [Test]
+        public async Task InsertUser()
+        {
+            // Arrange
+            var item = new User { Username = "username_test", Password = "password_test" };
+
+            // Act
+            await dataModel.InsertUserAsync(item).ConfigureAwait(true);
+
+            // Assert
+            Assert.AreEqual(1, dataModel.GetUsersListAsync().Result.Count);
+        }
+
+        [Test]
+        public async Task GetAllUsers()
+        {
+            // Arrange
+            var itemCount = 10;
+            for (var i = 0; i < itemCount; i++)
+            {
+                await dataModel.InsertUserAsync(new User { Username = "username_test_" + i, Password = "password_test" });
+            }
+
+            // Act
+            var items = await dataModel.GetUsersListAsync().ConfigureAwait(true);
+
+            // Assert
+            Assert.AreEqual(itemCount, items.Count);
+        }
+
+        [Test]
+        public async Task GetUser()
+        {
+            // Arrange
+            var item = new User { Username = "username_test", Password = "password_test" };
+            await dataModel.InsertUserAsync(item);
+
+            // Act
+            var item2 = await dataModel.GetUserAsync(item.Username).ConfigureAwait(true);
+
+            // Assert
+            Assert.True(item.Equals(item2));
+        }
+
+        [Test]
+        public async Task UpdateUser()
+        {
+            // Arrange
+            const string newPassword = "password_test_1";
+
+            var item = new User { Username = "username_test", Password = "password_test" };
+            await dataModel.InsertUserAsync(item);
+
+            // Act
+            item.Password = newPassword;
+            await dataModel.UpdateUserAsync(item);
+
+            var item2 = await dataModel.GetUserAsync(item.Username).ConfigureAwait(true);
+
+            // Assert
+            Assert.AreEqual(newPassword, item2.Password);
+        }
+
+        [Test]
+        public async Task DeleteUser()
+        {
+            // Arrange
+            var item = new User { Username = "username_test", Password = "password_test" };
+            await dataModel.InsertUserAsync(item);
+
+            // Act
+            await dataModel.DeleteUserAsync(item.Username).ConfigureAwait(true);
+
+            // Assert
+            Assert.AreEqual(0, dataModel.GetUsersListAsync().Result.Count);
+        }
+    }
+}
