446d42485175c6d2e71bc34e7d8ac2e5998fa567
zanuccod <54598430+zanuccod@users.noreply.github.com>
2020-01-08 22:07:33 +0100

    Add IdentityServer IntegrationTest project

---
+++ WebApi/Services/Identity/IdentityServer.API.IntegrationTest/PredefinedData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using IdentityModel;
+using IdentityServer.API.Domains;
+
+namespace IdentityServer.API.IntegrationTest
+{
+    public static class PredefinedData
+    {
+        public readonly static string Password = "demo";
+
+        public readonly static User[] Profiles = {
+            new User { Username = "demo", Password = Password.ToSha256() },
+            new User { Username = "tester@test.com", Password = Password },
+            new User { Username = "author@test.com", Password = Password }
+        };
+    }
+}
